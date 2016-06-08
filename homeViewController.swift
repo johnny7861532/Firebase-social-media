89f9666caf313998ffd8d6affbee7cf3bae7997e
johnny7861532 <johnny7861532@hotmail.com>
2016-06-08 15:28:29 +0800

    User now can change his profile photo in anytime!
    Finish changing photo by Firebase storage and Firebase database to
    record the URL and listen the change of userPhoto.

--- homeViewController.swift
+++ homeViewController.swift
@@ -16,9 +16,10 @@ class homeViewController: UIViewController,UIImagePickerControllerDelegate, UINa
     @IBOutlet weak var userPhoto: UIImageView!
 
     @IBOutlet weak var userNameLabel: UILabel!
+    let picker = UIImagePickerController()
     var databaseRef: FIRDatabaseReference!
     var storageRef: FIRStorageReference!
-
+    var tapDidChangePhoto: UIImage!
     
     @IBAction func tapDidLogOut(sender: AnyObject) {
     try!FIRAuth.auth()!.signOut()
@@ -31,13 +32,10 @@ class homeViewController: UIViewController,UIImagePickerControllerDelegate, UINa
         super.viewDidLoad()
       storageRef = FIRStorage.storage().reference()
       databaseRef = FIRDatabase.database().reference()
-        
-    }
-    
-    
-      
-    override func viewWillAppear(animated: Bool) {
-        if FIRAuth.auth()!.currentUser == nil{
+     //edit userphoto to circle
+      userPhoto.layer.cornerRadius = userPhoto.frame.size.height/2
+      userPhoto.clipsToBounds = true
+    if FIRAuth.auth()!.currentUser == nil{
             dispatch_async(dispatch_get_main_queue(), {() -> Void in
                 let viewController:UIViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("Login")
                 self.presentViewController(viewController, animated: true, completion: nil)
@@ -47,57 +45,82 @@ class homeViewController: UIViewController,UIImagePickerControllerDelegate, UINa
         }else{
             
             
-            let userID = FIRAuth.auth()?.currentUser?.uid
-            databaseRef.child("users").child(userID!).observeSingleEventOfType(.Value, withBlock: { (snapshot) in
+        let userID = FIRAuth.auth()?.currentUser?.uid
+         databaseRef.child("users").child(userID!).observeSingleEventOfType(.Value, withBlock: { (snapshot) in
                 // Get user value
                 let username = snapshot.value!["username"] as! String
-               
+                
                 self.userNameLabel.text = username
                 
+                
             })
-
+            //get photo back
+            
+         databaseRef.child("users").child(userID!).observeSingleEventOfType(.Value, withBlock: { (snapshot) in
+                // check if user has photo
+                if snapshot.hasChild("userPhoto"){
+                    // set image locatin
+                    let filePath = "\(userID!)/\("userPhoto")"
+                    // Assuming a < 10MB file, though you can change that
+                    self.storageRef.child(filePath).dataWithMaxSize(10*1024*1024, completion: { (data, error) in
+                        
+                        let userPhoto = UIImage(data: data!)
+                        self.userPhoto.image = userPhoto
+                    })
+                }
+            })
+            
         }
-}
+
+         }
+    
+    
+      
+    override func viewWillAppear(animated: Bool) {
+               }
+
     
     @IBAction func tapDidChangePhoto(sender: AnyObject) {
-        
-        var picker = UIImagePickerController()
-        // photo editing
-        picker.allowsEditing = true
-        picker.delegate = self
-        // if it's a photo from carmera else from photolibaray
-        if (UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)){
-        
-        picker.sourceType = .PhotoLibrary
-        }
-        presentViewController(picker, animated: true, completion:nil)
-        }
-    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
-        
-        // Get local file URLs
-        guard let image: UIImage = info[UIImagePickerControllerOriginalImage] as? UIImage else { return }
-        let imageData = UIImagePNGRepresentation(image)!
-        guard let imageURL: NSURL = info[UIImagePickerControllerReferenceURL] as? NSURL else { return }
-        
-        // Get a reference to the location where we'll store our photos
-       self.storageRef.child("userPhotos")
-        
-        // Get a reference to store the file at chat_photos/<FILENAME>
-        let photoRef = storageRef.child("\(NSUUID().UUIDString).png")
-        
-        // Upload file to Firebase Storage
-        let metadata = FIRStorageMetadata()
-        metadata.contentType = "image/png"
-        photoRef.putData(imageData, metadata: metadata).observeStatus(.Success) { (snapshot) in
-            // When the image has successfully uploaded, we get it's download URL
-            let text = snapshot.metadata?.downloadURL()?.absoluteString
-            // Set the download URL to the message box, so that the user can send it to the database
-            self.userPhoto.image = UIImage.init(data: imageData)
-        }
-        
-        // Clean up picker
+    let picker = UIImagePickerController()
+    picker.delegate = self
+    picker.allowsEditing = true
+    if UIImagePickerController.isSourceTypeAvailable(.Camera){
+    picker.sourceType = .Camera
+    }else{
+    picker.sourceType = .PhotoLibrary
+    }
+    self.presentViewController(picker, animated: true, completion: nil)
+    }
+    
+
+    //picker delegate
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+        userPhoto.image = image
         dismissViewControllerAnimated(true, completion: nil)
-    }
+        var data = NSData()
+        data = UIImageJPEGRepresentation(userPhoto.image!, 0.8)!
+        // set upload path
+        let filePath = "\(FIRAuth.auth()!.currentUser!.uid)/\("userPhoto")"
+        let metaData = FIRStorageMetadata()
+        metaData.contentType = "image/jpg"
+        self.storageRef.child(filePath).putData(data, metadata: metaData){(metaData,error) in
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }else{
+            //store downloadURL
+            let downloadURL = metaData!.downloadURL()!.absoluteString
+            //store downloadURL at database
+        self.databaseRef.child("users").child(FIRAuth.auth()!.currentUser!.uid).updateChildValues(["userPhoto": downloadURL])
+            }
+
+            }
+                       }
+    
+    
+    
+        
     }
 
 
+   
